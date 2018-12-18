5d9f172097d2ece32eea3955201bb0d0c202e613
alexgagnon <asgagnon@hotmail.com>
2018-12-18 07:42:16 -0500

    updates

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        var greeting = "Greetings from someone silly!!";
+        return greeting;
     }
-
 }
